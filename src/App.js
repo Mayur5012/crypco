e8dced2b5dd309533be372fc1c115c67a484f09d
Mayuresh Gawali <mayureshgawali9@gmail.com>
2022-08-27 12:45:06 +0530

    update 1

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import { Routes, Route } from 'react-router-dom'
 import Coins from './components/Coins'
 import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
-// import News from './components/News';
-// import LoadingBar from 'react-top-loading-bar'
-// import Crypnews from './components/crypnews'
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+import Crypnews from './components/crypnews'
 
 function App() {
-  // const pageSize = 5;
+  const pageSize = 5;
   const [coins, setCoins] = useState([])
 
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=50&page=1&sparkline=false'
-  // const apiKey= process.env.REACT_APP_NEWSdata_API
+  const apiKey= process.env.REACT_APP_NEWSdata_API
 
-  // const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0)
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data)
@@ -28,20 +28,20 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <LoadingBar
+      <LoadingBar
         height={3}
         color='#f11946'
         progress={progress} 
-      /> */}
+      />
       <Routes>
         <Route path='/crypto' element={<Coins coins={coins} />} />
         <Route path='/coin' element={<Coin />}>
           <Route path=':coinId' element={<Coin />} />
         </Route>
       {/* <Route path='/crypto'><News setProgress={setProgress} apiKey={apiKey} key="crypto" pageSize={pageSize} country="in" category="crypto"/></Route>  */}
-      {/* <Route path='/crypto' element={<News />}>
+      <Route path='/crypto' element={<News />}>
         <Route element={<News />} setProgress={setProgress} apiKey={apiKey} key="crypto" pageSize={pageSize} country="in" category="crypto" />
-      </Route>  */}
+      </Route> 
       </Routes>
 
     </>
